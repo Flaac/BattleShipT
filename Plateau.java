7afbd422613ee4da0327cc4e8784c05b716edf35
Flaac <Flaac@users.noreply.github.com>
2015-03-17 13:19:19 +0100

    Update Plateau.java

--- Plateau.java
+++ Plateau.java
@@ -1,43 +1,46 @@
 package v1;
 public class Plateau
 {
-private int ESSAIS[][]; //matrice qui donne les cases essayées '1=essayé'
-private int BATEAUX[][]; //matrice qui stocke la localisation des bateaux '1=bout de bateau'
-private int MER [][]; //matrice qui donne les emplacements de la mer -> si vide me jeu est fini et si touché la case redevient de la mer
-private int nbligne;
-private int nbcolonne;
-private Boat bateau1; //ajoute un bateau dans le plateau (est ce qu'on le met la ou on les ajoute après avoir créé le plateau avec add bateau?)
-Plateau(int nbligne, int nbcolonne)
-{
-this.nbligne = nbligne;
-this.nbcolonne = nbcolonne;
-ESSAIS = new int[nbligne][nbcolonne]; //initialisation de la matrice ESSAIS
-for(int i=0;i<nbligne;i++)
-{
-for(int j=0;j<nbcolonne;j++)
-{
-ESSAIS[i][j] = 0;
-}
-}
-BATEAUX = new int[nbligne][nbcolonne]; //initialisation de la matrice bateaux
-for(int i=0;i<nbligne;i++)
-{
-for(int j=0;j<nbcolonne;j++)
-{
-BATEAUX[i][j] = 0;
-}
-}
-Boat bateau1 = new Boat(4,1,2,2,"Titanic");
-for (int i=0;i<bateau1.getSize();i++)
-{BATEAUX [bateau1.get_xDebut()+i*bateau1.getDirection()] [bateau1.get_yDebut()+i*(1-bateau1.getDirection())]=1;} //On incrémente l'abscisse si horizontal et l'ordonnée si vertical, pour remplir la matrice
-MER = new int[nbligne][nbcolonne]; //initialisation de la matrice MER
-for(int i=0;i<nbligne;i++)
-{
-for(int j=0;j<nbcolonne;j++)
-{
-MER[i][j] = 0;
-}
-}
+  private int ESSAIS[][]; //matrice qui donne les cases essayées '1=essayé'
+  private int BATEAUX[][]; //matrice qui stocke la localisation des bateaux '1=bout de bateau'
+  private int MER [][]; //matrice qui donne les emplacements de la mer -> si vide me jeu est fini et si touché la case redevient de la mer
+  private int nbligne;
+  private int nbcolonne;
+  private Boat bateau1; //ajoute un bateau dans le plateau (est ce qu'on le met la ou on les ajoute après avoir créé le plateau avec add bateau?)
+  Plateau(int nbligne, int nbcolonne)
+  {
+    this.nbligne = nbligne;
+    this.nbcolonne = nbcolonne;
+    ESSAIS = new int[nbligne][nbcolonne]; //initialisation de la matrice ESSAIS
+    for(int i=0;i<nbligne;i++)
+    {
+      for(int j=0;j<nbcolonne;j++)
+      {
+        ESSAIS[i][j] = 0;
+      }
+    }
+  BATEAUX = new int[nbligne][nbcolonne]; //initialisation de la matrice bateaux
+  for(int i=0;i<nbligne;i++)
+  {
+    for(int j=0;j<nbcolonne;j++)
+    {
+      BATEAUX[i][j] = 0;
+    }
+  }
+  Boat bateau1 = new Boat(4,1,2,2,"Titanic");
+  for (int i=0;i<bateau1.getSize();i++)
+  {
+    BATEAUX [bateau1.get_xDebut()+i*bateau1.getDirection()] [bateau1.get_yDebut()+i*(1-bateau1.getDirection())]=1;
+    //On incrémente l'abscisse si horizontal et l'ordonnée si vertical, pour remplir la matrice
+  } 
+    MER = new int[nbligne][nbcolonne]; //initialisation de la matrice MER
+    for(int i=0;i<nbligne;i++)
+    {
+      for(int j=0;j<nbcolonne;j++)
+      {
+        MER[i][j] = 0;
+      }
+    }
 }
 public void add_boat (Boat b) //ajoute un bateau sur le plateau erreur si deux bateaux sont au même endroit
 {
